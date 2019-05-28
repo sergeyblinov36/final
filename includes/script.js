2bac19795c1530ede19512a303a296780e3ca83e
sergey <sergeyblinov36@gmail.com>
2019-05-28 06:07:06 +0300

    first commit

---
+++ includes/script.js
@@ -0,0 +1,92 @@
+
+/* for butoons on click */
+function pageLayout2() {
+    location.replace('listDogsPage.html');
+}
+
+function pageLayout3() {
+    location.replace('AddDogPage.html');
+}
+
+function pageIndex() {
+    location.replace('index.html');
+}
+
+function myDogINFO() {
+    location.replace('myDoginfoPage.html');
+}
+
+function goBackTOindex() {
+    location.replace('index.html');
+}
+
+function goBackTOlist() {
+    location.replace('listDogsPage.html');
+}
+
+
+
+/*for galary - page 'myDoginfoPage.html' */
+jQuery(document).ready(function($) {
+ 
+    $('#myCarousel').carousel({
+            interval: 5000
+    });
+
+    $('#carousel-text').html($('#slide-content-0').html());
+
+    //Handles the carousel thumbnails
+   $('[id^=carousel-selector-]').click( function(){
+        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+    });
+
+
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+             var id = $('.item.active').data('slide-number');
+            $('#carousel-text').html($('#slide-content-'+id).html());
+    });
+});
+/*----------------------------------------------------------------*/
+
+var audio;
+var playlist;
+var tracks;
+var current;
+
+init();
+function init(){
+    current = 0;
+    audio = $('#audio');
+    playlist = $('#playlist');
+    tracks = playlist.find('li a');
+    len = tracks.length - 1;
+    audio[0].volume = 0.5;
+    audio[0].play();
+    playlist.find('a').click(function(e){
+        e.preventDefault();
+        link = $(this);
+        current = link.parent().index();
+        run(link, audio[0]);
+    });
+    audio[0].addEventListener('ended',function(e){
+        current++;
+        if(current == len){
+            current = 0;
+            link = playlist.find('a')[0];
+        }else{
+            link = playlist.find('a')[current];    
+        }
+        run($(link),audio[0]);
+    });
+}
+function run(link, player){
+        player.src = link.attr('href');
+        par = link.parent();
+        par.addClass('active').siblings().removeClass('active');
+        audio[0].load();
+        audio[0].play();
+}
+
